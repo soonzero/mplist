5978d57da58735a9328e99bc0a388c583faf2887
soonzero <sykeum0912@naver.com>
2022-06-07 22:46:34 +0900

    add: @svgr/webpack 설치

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ["i.scdn.co"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$s/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
