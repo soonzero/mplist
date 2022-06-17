d271e3b1b2a915f0812bfac954ac717ecb3f1f01
soonzero <sykeum0912@naver.com>
2022-06-18 02:01:53 +0900

    refactor: 플레이리스트 관련 함수 분리

--- functions/playlists.js
+++ functions/playlists.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import useAPI from "./common";
+import Cookies from "js-cookie";
 
 export const createPlaylist = async (token, id, name, description, checked) => {
   try {
@@ -15,6 +17,74 @@ export const createPlaylist = async (token, id, name, description, checked) => {
       },
     });
     return response.data.id;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const changePlaylistDetail = async (
+  e,
+  router,
+  token,
+  id,
+  name,
+  description,
+  checked
+) => {
+  try {
+    e.preventDefault();
+    await axios({
+      method: "PUT",
+      url: `https://api.spotify.com/v1/playlists/${id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        name,
+        description,
+        public: checked,
+      },
+    });
+    router.push("/mypage/my-playlists");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const managePlaylistFollowing = async (id, follow, setFollow) => {
+  try {
+    await useAPI(
+      Cookies.get("mplistToken"),
+      `${follow ? "DELETE" : "PUT"}`,
+      `/playlists/${id}/followers`
+    );
+    setFollow(!follow);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const unfollowPlaylist = async (playlistId, setMyPlaylists) => {
+  try {
+    await useAPI(
+      Cookies.get("mplistToken"),
+      "DELETE",
+      `/playlists/${playlistId}/followers`
+    );
+    getMyPlaylists(setMyPlaylists);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getMyPlaylists = async (setMyPlaylists) => {
+  try {
+    const data = await useAPI(
+      Cookies.get("mplistToken"),
+      "GET",
+      `/me/playlists`
+    );
+    setMyPlaylists(data.items);
   } catch (e) {
     console.log(e);
   }
--- pages/mypage/my-playlists.js
+++ pages/mypage/my-playlists.js
@@ -1,69 +1,58 @@
-import Cookies from "js-cookie";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
-import { useAPI } from "../../components/useAPI";
 import DeleteSVG from "../../public/delete.svg";
+import Link from "next/link";
+import { unfollowPlaylist, getMyPlaylists } from "../../functions/playlists";
 
 const MyPlaylist = () => {
-  const token = Cookies.get("mplistToken");
-  const [data, setData] = useState();
-
-  const unfollowPlaylist = async (playlistId) => {
-    try {
-      await useAPI(token, "DELETE", `/playlists/${playlistId}/followers`);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const getData = async () => {
-    try {
-      const response = await useAPI(token, "GET", `me/playlists`);
-      console.log(response);
-      setData(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const [myPlaylists, setMyPlaylists] = useState([]);
 
   useEffect(() => {
-    getData();
+    getMyPlaylists(setMyPlaylists);
   }, []);
 
   return (
     <Layout title="내 플레이리스트">
-      {data && (
-        <div className="py-4 flex flex-col">
-          <h1 className="font-bold text-3xl mb-5">내 플레이리스트</h1>
-          <ul className="divide-y">
-            {data.items.map((i) => {
-              return (
-                <li key={i.id} className="py-5 px-3 flex">
-                  <Image
-                    className="mr-5"
-                    src={i.images[0]?.url || `/logo-no-text.svg`}
-                    width={100}
-                    height={100}
-                  />
-                  <div className="grow mx-5">
-                    <h3>{i.name}</h3>
-                    <p>{i.owner.display_name}</p>
+      <div className="py-4 flex flex-col">
+        <h1 className="font-bold text-3xl mb-5">내 플레이리스트</h1>
+        <ul className="divide-y">
+          {myPlaylists?.map((i) => {
+            return (
+              <li
+                key={i.id}
+                className="py-5 px-3 flex cursor-pointer hover:bg-slate-200 relative"
+              >
+                <Link href={`/playlists/${i.id}`}>
+                  <div className="flex w-full">
+                    <Image
+                      className="mr-5"
+                      src={i.images[0]?.url || `/logo-no-text.svg`}
+                      width={100}
+                      height={100}
+                    />
+                    <div className="grow px-5">
+                      <h3 className="text-lg font-semibold">{i.name}</h3>
+                      <p>{i.owner.display_name}</p>
+                      <span className="text-sm text-gray-500">
+                        트랙 {i.tracks.total}개
+                      </span>
+                    </div>
                   </div>
-                  <div className="flex items-center">
-                    <span
-                      className="h-max cursor-pointer hover:text-mplist"
-                      onClick={() => unfollowPlaylist(i.id)}
-                    >
-                      <DeleteSVG className="w-6 h-6" />
-                    </span>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
+                </Link>
+                <div className="px-3 absolute right-0 top-1/2 -translate-y-1/2 flex items-center">
+                  <span
+                    className="h-max cursor-pointer hover:text-mplist"
+                    onClick={() => unfollowPlaylist(i.id, setMyPlaylists)}
+                  >
+                    <DeleteSVG className="w-6 h-6" />
+                  </span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </Layout>
   );
 };
