6b01cc27d48568615374650a86514a93c329a5fe
soonzero <sykeum0912@naver.com>
2022-06-19 00:29:49 +0900

    fix: 기본 레이아웃 수정(비 로그인 시 api 호출 시도x)

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,21 @@
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Link from "next/link";
+import AUTH_LINK from "../data/api";
 
 const Layout = ({ title, children }) => {
+  const [display, setDisplay] = useState();
+
+  useEffect(() => {
+    if (Cookies.get("mplistToken")) {
+      setDisplay(true);
+    } else {
+      setDisplay(false);
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -9,7 +23,18 @@ const Layout = ({ title, children }) => {
       </Head>
       <NavBar />
       <main className="flex flex-col max-w-screen-2xl mx-auto px-8">
-        {children}
+        {display ? (
+          children
+        ) : (
+          <div className="text-4xl flex items-center justify-center h-[calc(100vh-64px)]">
+            <Link href={AUTH_LINK}>
+              <a className="font-bold hover:text-mplist">
+                로그인 후에 이용 가능합니다. <br />
+                여기를 눌러 로그인해주세요.
+              </a>
+            </Link>
+          </div>
+        )}
       </main>
     </>
   );
