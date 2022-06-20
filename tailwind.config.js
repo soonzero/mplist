b4dde72d85c91cd09835b6047092c1ec0b2808aa
soonzero <sykeum0912@naver.com>
2022-06-20 23:01:53 +0900

    docs: tailwind.config.js에 customized spin animation 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
     extend: {
+      animation: {
+        "spin-slow": "spin 1.5s linear infinite",
+      },
       colors: {
         mplist: "#D2CAF6",
         spotify: "#1ED760",
