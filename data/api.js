7520b7624431a1a8739d21af638b75a6fe2879ca
soonzero <sykeum0912@naver.com>
2022-06-22 01:17:21 +0900

    docs: redirect_uri 수정

--- data/api.js
+++ data/api.js
@@ -3,10 +3,8 @@ const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
 const AUTH_ENDPOINT = process.env.NEXT_PUBLIC_AUTH_ENDPOINT;
 const RESPONSE_TYPE = process.env.NEXT_PUBLIC_RESPONSE_TYPE;
 const SCOPE = process.env.NEXT_PUBLIC_SCOPE;
-const AUTH_LINK = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+const AUTH_LINK = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
 export default AUTH_LINK;
 
 // 2022.06.21 mplist.netlify.app deploy 하면서 AUTH_LINK 변경
-
-// ${REDIRECT_URI}
