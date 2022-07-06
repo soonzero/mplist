f9bc451a54f479f4b9dc1b0b544beb4e4f117263
soonzero <sykeum0912@naver.com>
2022-07-06 15:27:29 +0900

    fix: SSR 안 되던 부분 해결

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -6,16 +6,16 @@ import Link from "next/link";
 import AUTH_LINK from "../../data/api";
 import Footer from "./Footer";
 
-const Layout = ({ title, children }) => {
-  const [display, setDisplay] = useState();
-
-  useEffect(() => {
-    if (Cookies.get("mplistToken")) {
-      setDisplay(true);
-    } else {
-      setDisplay(false);
+export const getServerSideProps = (context) => {
+  const token = context.req.cookies["mplistToken"]
+  return {
+    props: {
+      token
     }
-  }, []);
+  }
+}
+
+const Layout = ({ title, children }) => {
 
   return (
     <>
@@ -24,18 +24,17 @@ const Layout = ({ title, children }) => {
       </Head>
       <Header />
       <main className="flex flex-col max-w-screen-2xl mx-auto px-8 mobile:px-4 tablet:px-8">
-        {display ? (
-          children
-        ) : (
-          <section className="text-4xl flex items-center justify-center h-[calc(100vh-64px)]">
-            <Link href={AUTH_LINK}>
-              <a className="font-bold hover:text-mplist mobile:text-base tablet:text-2xl laptop:text-3xl monitor:text-4xl text-center">
-                로그인 후에 이용 가능합니다. <br />
-                여기를 눌러 로그인해주세요.
-              </a>
-            </Link>
-          </section>
-        )}
+          {children}
+        {/* // : (
+        //   <section className="text-4xl flex items-center justify-center h-[calc(100vh-64px)]">
+        //     <Link href={AUTH_LINK}>
+        //       <a className="font-bold hover:text-mplist mobile:text-base tablet:text-2xl laptop:text-3xl monitor:text-4xl text-center">
+        //         로그인 후에 이용 가능합니다. <br />
+        //         여기를 눌러 로그인해주세요.
+        //       </a>
+        //     </Link>
+        //   </section>
+        // ) */}
       </main>
       <Footer />
     </>
